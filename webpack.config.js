ba7de8ecf4ac40f9974ed112d788ae705bb5bb3f
Trapti Rahangdale <imtrapti1410@gmail.com>
2019-03-23 21:06:43 +0530

    initial setup commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js"
+  }, 
+  module: {
+    rules: [
+      { test: /\.(js)$/,use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({ template: 'src/index.html'})
+  ],
+  mode: 'development'
+}
