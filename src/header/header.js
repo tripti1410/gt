81c63bacabed46184ee4d37f6ea5cc9fdc7b187f
Trapti Rahangdale <imtrapti1410@gmail.com>
2019-05-02 00:24:55 +0530

    basec layout skeleton

---
+++ src/header/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header className="main-header">
+      <a href="#" className="main-header__title"><h1>Finding Falcone</h1></a>
+      <div className="main-header__links">
+        <a href="#" className="main-header__link">Reset</a>
+        <a href="#" className="main-header__link">GeekTrustHome</a>
+      </div>
+    </header> 
+  )
+}
+
+export default Header;
